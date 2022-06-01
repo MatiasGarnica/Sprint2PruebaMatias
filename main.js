8325fd9685dff18bee484baba1ce1a93481f339a
Matias Garnica <garnicamatias@outlook.es>
2022-06-01 14:49:37 -0300

    Casi-final

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
+var arrayInput = [];
+var arrayName = [];
+let sum = 0;
+let total = 0;
+let text = "";
+
 function Enviar(){
-    var arrayInput = new Array();
-    var arrayName = new Array();
-    var inputsValue = document.getElementsByClassName("gasto");
-    var inputsName = document.getElementsByClassName("nombre");
-    namesValues = [].map.call(inputsValue,function(dataInput){
-        arrayInput.push(dataInput.value);
-    });
-    namesNames = [].map.call(inputsName,function(dataName){
-        arrayName.push(dataName.value);    
-    });
-    
-    arrayInput.forEach(function(inputsValuesData){
-        arrayName.forEach(function(inputsNamesData){
-            document.getElementById("Total").innerHTML = inputsNamesData +": $"+ inputsValuesData 
-        })
-    })
-    for (const x of arrayName){
-        html += `<li>${x}</li>`;
+    inputsValue = document.getElementById("gasto").value;
+    inputsName = document.getElementById("nombre").value;
+    arrayInput.push(inputsValue);
+    arrayName.push(inputsName);
+
+    for(let i = 0; i < arrayName.length; i++){
+        sum = parseFloat(arrayInput[i]);
+        text += arrayName[i] + ": $" + arrayInput[i] + "<br>";
+        document.getElementById("Lista").innerHTML = text;
     }
-    document.getElementById("Total").innerHTML = html;
-}
+
+    
+    total += parseFloat(sum.toFixed(2))
+
+
+    document.getElementById("Total").innerHTML = "Total: $" + total
+    document.getElementById("Division").innerHTML = "A cada uno le toca aportar: $" + ((total / arrayInput.length).toFixed(2))
+
+} 
+
 
 
