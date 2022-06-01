19763d09bec44f3d768dfe678bc4cb598752c6dd
Matias Garnica <garnicamatias@outlook.es>
2022-06-01 15:16:03 -0300

    Confirmado

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ var arrayName = [];
 let sum = 0;
 let total = 0;
 let text = "";
+let vacio = "";
 
 function Enviar(){
+    document.getElementById("Lista").innerHTML = vacio;
     inputsValue = document.getElementById("gasto").value;
     inputsName = document.getElementById("nombre").value;
     arrayInput.push(inputsValue);
@@ -15,7 +17,7 @@ function Enviar(){
         text += arrayName[i] + ": $" + arrayInput[i] + "<br>";
         document.getElementById("Lista").innerHTML = text;
     }
-
+    text = ""
     
     total += parseFloat(sum.toFixed(2))
 
